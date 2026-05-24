dcb8131cd982356684fd921e1d1f7c5ab15a746c
Mohammad-Labib <friendlabib@gmail.com>
2026-05-25 01:07:52 +0600

    vercel add and deployed

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -2,7 +2,8 @@ import { createAuthClient } from "better-auth/react"
 import { jwtClient } from "better-auth/client/plugins"
 export const authClient = createAuthClient({
     
-    baseURL: process.env.BETTER_AUTH_URL,
+    baseURL: process.env.BETTER_AUTH_URL || process.env.NEXT_PUBLIC_BETTER_AUTH_URI,
+    plugins:[jwtClient()],
 });
 
 export const { signIn, signUp, signOut, useSession } = createAuthClient();
